b8c5bd53ec4135d4b9d6351da54eeaa7a5b58a06
Markas Korotkovas <mkorotkovas@yahoo.com>
2016-05-08 13:19:34 -0600

    added screensaver disable to run script

--- runGooglePhotosFrame.sh
+++ runGooglePhotosFrame.sh
@@ -5,6 +5,11 @@ git submodule update --recursive --init
 echo clean up logs
 rm -f gfp.log
 
+echo disabling screen saver
+xset s noblank 
+xset s off 
+xset -dpms
+
 echo Running the app
 gradle execute > gpf.log
 sleep 30
