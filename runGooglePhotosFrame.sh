3c6548bd61a52b168f0b55b097b052df1532106e
Markas Korotkovas <markas.korotkovas@efirstbank.com>
2016-05-10 16:15:00 +0100

    updated run script to pull latest Common

--- runGooglePhotosFrame.sh
+++ runGooglePhotosFrame.sh
@@ -1,6 +1,7 @@
 echo Getting latest version from GitHub...
 git pull
 git submodule update --recursive --init
+git submodule foreach git pull origin master
 
 echo disabling screen saver
 xset s noblank 
