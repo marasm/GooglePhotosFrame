4ca333de027c7e0e34d4f58f0815f3734d8ebba7
Markas Korotkovas <mkorotkovas@yahoo.com>
2017-10-28 18:26:54 -0600

    updated the run script

--- runGooglePhotosFrame.sh
+++ runGooglePhotosFrame.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+echo Making sure the current working directory is correct
+cd "$(dirname "$0")" 
+
 echo Getting latest version from GitHub...
 git pull
 git submodule update --recursive --checkout --force --init
